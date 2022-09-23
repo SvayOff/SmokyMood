0cf5e5e411d5a3aa20f4346acbc217028ccf77cb
SvayOff <marsiul2001@gmail.com>
2022-09-23 12:03:53 +0300

    added team section

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,32 @@
-$(function () {
+"use strict";
+
+const tabsPerent = document.querySelector('.choose__tabs'),
+  tabs = document.querySelectorAll('.choose__tab'),
+  tabsContent = document.querySelectorAll('.choose__tab-content');
+
+function hideTabsContent() {
+  tabs.forEach(tab => tab.classList.remove('active'));
+  tabsContent.forEach(tabContent => tabContent.classList.add('hide'));
+}
+
+hideTabsContent();
+
+function showTabsContent(i = 0) {
+  tabs[i].classList.add('active');
+
+  tabsContent[i].classList.remove('hide');
+  tabs.content[i].classList.add('show');
+}
+
+showTabsContent()
+
+tabsPerent.addEventListener('click', (event) => {
+  const target = event.target;
+  tabs.forEach(tab => {
+    if (target && tab === target) {
+      hideTabsContent();
+      showTabsContent(i);
+    }
+  });
 });
+
