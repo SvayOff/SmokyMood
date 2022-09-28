d948039f31911914932175ae63374c198f6eb734
SvayOff <marsiul2001@gmail.com>
2022-09-28 11:51:06 +0300

    added about page

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,17 @@ $(function () {
     dots: true
   });
 
+
+  $('.sponsors__slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
 });
 
 
